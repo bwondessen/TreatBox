41f261f273901140fdc59115feab898190447da5
bwondessen <bwondessen23@gmail.com>
2024-09-24 18:44:08 -0500

    Initial Commit

---
+++ TreatBox/TreatBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreatBoxApp.swift
+//  TreatBox
+//
+//  Created by Bruke Wondessen on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TreatBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
