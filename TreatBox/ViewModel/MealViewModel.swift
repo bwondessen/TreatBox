8f1e175e109f344c79f7af1a88de841f78a2a69e
bwondessen <bwondessen23@gmail.com>
2024-09-25 23:25:19 -0500

    Update fetchMeals to use Combine for network calls

--- TreatBox/ViewModel/MealViewModel.swift
+++ TreatBox/ViewModel/MealViewModel.swift
@@ -6,39 +6,49 @@
 //
 
 import Foundation
+import Combine
 
 // MARK: - Meal ViewModel
 class MealViewModel: ObservableObject {
     @Published var meals: [Meal] = [Meal]()
     
+    var cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
+    
+    
     func fetchMeals() async throws {
         // Endpoint
-        let endpoint = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
+        let endpoint: String = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
         
         // Validate and create URL
         guard let url = URL(string: endpoint) else {
             throw NetworkingError.invalidURL
         }
         
-        do {
-            // Fetch data from URL
-            let (data, response) = try await URLSession.shared.data(from: url)
-            
-            // Validate HTTP response status code
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                throw NetworkingError.invalidResponse
-            }
-            
-            // Decode data from URL
-            if let data = try? JSONDecoder().decode(Meals.self, from: data) {
-                // Update UI on main thread
-                DispatchQueue.main.async {
-                    self.meals = data.meals
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background)) // Download on background thread
+            .receive(on: DispatchQueue.main) // Receive on main thread
+            .tryMap(handleOutput)
+            .decode(type: Meals.self, decoder: JSONDecoder()) // Decode data from URL
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error downloading data: \(error.localizedDescription)")
                 }
+            } receiveValue: { [weak self] data in
+                self?.meals = data.meals
             }
-        } catch {
-            throw NetworkingError.invalidData
+            .store(in: &cancellables)
+    }
+    
+    private func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        // Validate HTTP URL response status code
+        guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode <= 300 else {
+            throw NetworkingError.invalidResponse
         }
+        
+        return output.data
     }
 }
 
