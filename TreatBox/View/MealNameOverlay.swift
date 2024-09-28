5445d2cc930cccf45c876a97ecd0af4818c0ddc1
bwondessen <bwondessen23@gmail.com>
2024-09-27 22:22:40 -0500

    Extract subviews

---
+++ TreatBox/View/MealNameOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  MealNameOverlay.swift
+//  TreatBox
+//
+//  Created by Bruke Wondessen on 9/27/24.
+//
+
+import SwiftUI
+
+struct MealNameOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealNameOverlay()
+}
---
+++ TreatBox/View/YouTubeLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  YouTubeLinkView.swift
+//  TreatBox
+//
+//  Created by Bruke Wondessen on 9/27/24.
+//
+
+import SwiftUI
+
+struct YouTubeLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTubeLinkView()
+}
