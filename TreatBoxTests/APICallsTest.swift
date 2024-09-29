c6ad5e6ca8f0222c00b9f0061690739892b51b15
bwondessen <bwondessen23@gmail.com>
2024-09-29 02:50:37 -0500

    Implement unit testing and fix bugs

---
+++ TreatBoxTests/APICallsTest.swift
@@ -0,0 +1,16 @@
+//
+//  APICallsTest.swift
+//  TreatBoxTests
+//
+//  Created by Bruke Wondessen on 9/28/24.
+//
+
+import Testing
+
+struct APICallsTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
