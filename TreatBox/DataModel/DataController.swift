294ddd36898d6167f7b830eb6321b7f8438a739f
bwondessen <bwondessen23@gmail.com>
2024-10-01 05:30:09 -0500

    Persist data with Core Data and create tab view for book mark view

---
+++ TreatBox/DataModel/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  TreatBox
+//
+//  Created by Bruke Wondessen on 9/30/24.
+//
+
+import Foundation
---
+++ TreatBox/View/BookmarkView.swift
@@ -0,0 +1,18 @@
+//
+//  BookmarkView.swift
+//  TreatBox
+//
+//  Created by Bruke Wondessen on 9/30/24.
+//
+
+import SwiftUI
+
+struct BookmarkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookmarkView()
+}
